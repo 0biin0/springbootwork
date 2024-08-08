ce97b3a0b602bb97b0145eb1cd9c3ceff38af851
hyeyeon9037 <infinite402@gmail.com>
2024-08-08 18:56:37 +0900

    0808

--- Ex08_MemberJpa01/src/main/java/com/study/springboot/domain/Member.java
+++ Ex08_MemberJpa01/src/main/java/com/study/springboot/domain/Member.java
@@ -1,5 +1,27 @@
 package com.study.springboot.domain;
 
-public class Member {
+import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="member01")
+public class Member {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String username;
+	@Column(name="create_date") //컬럼 이름 따로 정해주는것
+	private LocalDate createDate;
 }
--- Ex08_MemberJpa01/src/main/java/com/study/springboot/service/MemberService.java
+++ Ex08_MemberJpa01/src/main/java/com/study/springboot/service/MemberService.java
@@ -1,5 +1,21 @@
 package com.study.springboot.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.study.springboot.domain.Member;
+import com.study.springboot.repository.MemberRepository;
+
+@Service
 public class MemberService {
+	
+	@Autowired
+	MemberRepository memberRepository;
+	
+	public Member insert(Member member) {
+		// save() : insert할 떄의 메소드(JPA에 API)
+		Member rmember =memberRepository.save(member);
+		return rmember;
+	}
 
 }
