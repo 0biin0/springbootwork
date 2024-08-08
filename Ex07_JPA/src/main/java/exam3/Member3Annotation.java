07c0d42c0c291b7812d7f7d25773bcf0fc562a20
hyeyeon9037 <infinite402@gmail.com>
2024-08-08 15:28:18 +0900

    0808

--- Ex07_JPA/src/main/java/exam3/Member3Annotation.java
+++ Ex07_JPA/src/main/java/exam3/Member3Annotation.java
@@ -1,38 +1,31 @@
-package exam1;
-
-import java.time.LocalDate;
+package exam3;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
-public class MemberUse {
+public class Member3Annotation {
 
 	public static void main(String[] args) {
 		EntityManagerFactory emf =Persistence.createEntityManagerFactory("JpaEx01");
 		EntityManager entityManager = emf.createEntityManager();
 		EntityTransaction transaction = entityManager.getTransaction();
-		
+
 		try {
-		transaction.begin();
-		
-		Member1 user = new Member1("홍길동1", LocalDate.now());
-		
-		// .persist는 영속성으로 객체에 데이터 입력(메모리에 insert해주는 부분)
-		entityManager.persist(user);
-		
-		// 실제 DB에 insert
-		transaction.commit();
-		} catch(Exception e) {
+			transaction.begin();
+			
+			Member3 user = new Member3("아무개3", "1234");
+			entityManager.persist(user);
+			
+			transaction.commit();
+		}catch(Exception e){
 			e.printStackTrace();
 			transaction.rollback();
-		} finally {
+		}finally {
 			entityManager.close();
 			emf.close();
-			
 		}
 	}
 
 }
-
