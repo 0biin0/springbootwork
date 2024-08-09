f7eb054ecaf96c1bf161bff8638602a36a3351e6
hyeyeon9037 <infinite402@gmail.com>
2024-08-09 11:04:39 +0900

    0809

--- Ex08_MemberJpa01/src/main/java/com/study/springboot/service/MemberService.java
+++ Ex08_MemberJpa01/src/main/java/com/study/springboot/service/MemberService.java
@@ -1,5 +1,8 @@
 package com.study.springboot.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +14,28 @@ public class MemberService {
 	
 	@Autowired
 	MemberRepository memberRepository;
-	
+
 	public Member insert(Member member) {
 		// save() : insert할 때의 메소드(JPA에 API)
-		Member rmember =memberRepository.save(member);
-		return rmember;
+		Member rMember = memberRepository.save(member);
+		return rMember;	
+	}
+	
+	/*
+	 * Optional<T> : NullpointerException 발생을 방지하기 위해 사용
+	 * 				 기존의 반환 값에 타입T를 Optional<T> Wrapping하여, 
+	 *                    null 대신 Optional안에 빈 타입 객체를 돌려주는 기법
+	 *     ex) Member member = memberRepository.findById(id)
+	 *         member.getId();   => NullpointerException 발생
+	 */
+	public Optional<Member> select(Long id) {
+												// findById() : Id는 @Id를 의미(primary key)
+		Optional<Member> member = memberRepository.findById(id);
+		return member;
+	}
+
+	public List<Member> selectAll() {
+		return memberRepository.findAll();
 	}
 
 }
