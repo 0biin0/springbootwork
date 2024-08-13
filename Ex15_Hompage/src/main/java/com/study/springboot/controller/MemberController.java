e87f7cff9074aee97c1a5339af36b02921e6ba72
hyeyeon9037 <infinite402@gmail.com>
2024-08-13 17:14:11 +0900

    0813

--- Ex15_Hompage/src/main/java/com/study/springboot/controller/MemberController.java
+++ Ex15_Hompage/src/main/java/com/study/springboot/controller/MemberController.java
@@ -1,13 +1,21 @@
 package com.study.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.study.springboot.domain.Member;
+import com.study.springboot.service.MemberService;
+
 @Controller
 public class MemberController {
+	
+	@Autowired
+	MemberService memberService;
 	
 	@RequestMapping("/")
 	public String root() {
@@ -20,7 +28,14 @@ public class MemberController {
 	}
 	
 	@GetMapping("/idCheck")
+	/*@ResponseBody 여기다가 해줘도 됨*/
 	public @ResponseBody boolean idCheck(@RequestParam("id") String id) {
-		return false;
+		return memberService.idCheck(id);
+	}
+	
+	@PostMapping("/memberInsert")
+	public String memberInsert(Member member) {
+		memberService.memberInsert(member);
+		return "redirect:/";
 	}
 }
