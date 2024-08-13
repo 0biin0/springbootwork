3e5f13620101d1206eab58e174c5efb7cfe60d6c
hyeyeon9037 <infinite402@gmail.com>
2024-08-13 16:15:23 +0900

    0813

--- Ex15_Hompage/src/main/java/com/study/springboot/controller/MemberController.java
+++ Ex15_Hompage/src/main/java/com/study/springboot/controller/MemberController.java
@@ -1,12 +1,26 @@
 package com.study.springboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MemberController {
+	
 	@RequestMapping("/")
 	public String root() {
 		return "index";
+	}
+	
+	@GetMapping("/enrollForm")
+	public String enrollForm() {
+		return "member/enrollForm";
+	}
+	
+	@GetMapping("/idCheck")
+	public @ResponseBody boolean idCheck(@RequestParam("id") String id) {
+		return false;
 	}
 }
